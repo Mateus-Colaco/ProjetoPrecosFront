eef106548addc230ec6129d46d9c1714349a891e
Mateus Colaço <69285572+Mateus-Colaco@users.noreply.github.com>
2022-10-21 11:06:48 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    chunkSizeWarningLimit: 5000,
+    chunkSizeWarningLimit: 10000,
   },
   plugins: [react()]
 })
